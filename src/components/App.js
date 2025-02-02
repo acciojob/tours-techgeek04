48dcdfe0ff531eb445f18be56e29115716dd951e
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2025-02-02 22:42:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -66,13 +66,13 @@ const Tour = ({ id, name, info, image, price, removeTour }) => {
       <img src={image} alt={name} />
       <h2>{name}</h2>
       <h4>${price}</h4>
-      <p id={`tour-item-para-${id}`}>
+      <p id={`tour-item-para-${id}`} data-testid={`tour-item-para-${id}`}>
         {readMore ? info : `${info.substring(0, 200)}...`}
         <button onClick={() => setReadMore(!readMore)}>
           {readMore ? "Show less" : "Show more"}
         </button>
       </p>
-      <button id={`delete-btn-${id}`} onClick={() => removeTour(id)}>
+      <button id={`delete-btn-${id}`} data-testid={`delete-btn-${id}`} onClick={() => removeTour(id)}>
         Not Interested
       </button>
     </div>
